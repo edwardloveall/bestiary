ff9d7400de7cb2e1d6880228a7b42b2ea2d75693
Edward Loveall <edward@edwardloveall.com>
2016-07-07 09:58:57 -0400

    Refactor main to use basic attribute class

--- main.rb
+++ main.rb
@@ -1,24 +1,69 @@
+$LOAD_PATH.unshift(Dir.pwd)
 require 'bundler/setup'
 require 'nokogiri'
 require 'pg'
 require 'net/http'
-require 'pry'
+require 'attributes'
+require 'monster'
 
-uri = URI('http://paizo.com/pathfinderRPG/prd/bestiary3/baykok.html')
-response = Net::HTTP.get(uri)
+# uri = URI('http://paizo.com/pathfinderRPG/prd/bestiary3/baykok.html')
+# response = Net::HTTP.get(uri)
+response = File.read('baykok.html')
 parsed = Nokogiri::HTML(response)
 
 title_group = parsed.at('.stat-block-title')
 
-title = title_group.children.first.text.strip
+name = title_group.children.first.text.strip
 cr = title_group.at('.stat-block-cr').text.gsub(/(CR )/, '').to_i
-xp = parsed.at('.stat-block-xp').text.gsub(/(XP |,)/, '').to_i
-attrs = [title, cr, xp]
+
+xp_ele = parsed.at('.stat-block-xp')
+xp = xp_ele.text.gsub(/(XP |,)/, '').to_i
+
+stat_block = xp_ele.next_element
+alignment, size, type = stat_block.text.split(' ')
+
+stat_block = stat_block.next_element
+init_label = stat_block.css('b').select { |ele| ele.text == 'Init' }.first
+senses_label = stat_block.css('b').select { |ele| ele.text == 'Senses' }.first
+percept_label = stat_block.css('a').select do |ele|
+  ele.text == 'Perception'
+end.first
+init = init_label.next_element.text.gsub(/[^\d|\-]/, '').to_i
+senses = senses_label.next.text
+perception = percept_label.next.text.gsub(/[^\d|\-]/, '').to_i
+
+basic = Attributes::Basic.new(
+  name: name,
+  cr: cr,
+  xp: xp,
+  alignment: alignment,
+  size: size,
+  type: type,
+  initiative: init,
+  senses: senses,
+  perception: perception
+)
+
+monster = Monster.new(basic: basic)
 
 db = PG.connect(dbname: 'bestiary')
+
+value_placeholders = (1..basic.attributes.values.count).map { |n| "$#{n}" }
+sql = <<-SQL
+  INSERT INTO basic_attrs
+  (#{basic.attributes.keys.join(', ')})
+  VALUES (#{value_placeholders.join(', ')})
+  RETURNING id
+SQL
+
+result = db.exec_params(sql, basic.attributes.values)
+basic_id = result.field_values('id').first
+
 sql = <<-SQL
   INSERT INTO monsters
-  (title, cr, xp)
-  VALUES ($1, $2, $3)
+  (basic_attrs_id)
+  VALUES (#{basic_id})
 SQL
-db.exec_params(sql, attrs)
+
+db.exec(sql)
+result.field_values('id').first
