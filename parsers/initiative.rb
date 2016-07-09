3a8b22863c83bcaa1e4e25233cbe6ec1d4175fd8
Edward Loveall <edward@edwardloveall.com>
2016-07-09 15:55:15 -0400

    Add initiative parser

---
+++ parsers/initiative.rb
@@ -0,0 +1,12 @@
+class Parsers::Initiative
+  def self.perform(creature)
+    bold_elements = creature.css('b')
+    bold_elements.each do |element|
+      text = element.text.strip
+      if text == 'Init'
+        modifier = element.next.text
+        return modifier.gsub(/[^\d|\-]/, '').to_i
+      end
+    end
+  end
+end
