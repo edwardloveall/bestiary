7c822f847f330c685b9a497d8da6f5ce85b420ac
Edward Loveall <edward@edwardloveall.com>
2016-07-14 17:59:12 -0400

    Add pry to tests for debugging

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ $LOAD_PATH.unshift('../lib', __FILE__)
 require 'rspec'
 require 'bestiary'
 require 'nokogiri'
+require 'pry'
 
 Dir['spec/support/**/*.rb'].each { |file| require File.expand_path(file) }
 
