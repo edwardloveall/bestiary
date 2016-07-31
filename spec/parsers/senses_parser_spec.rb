56af2a6a12e6e02653b2b89e1e2c64e7feb73af0
Edward Loveall <edward@edwardloveall.com>
2016-07-30 20:43:35 -0400

    Add tests for senses parser

--- spec/parsers/senses_parser_spec.rb
+++ spec/parsers/senses_parser_spec.rb
@@ -9,10 +9,11 @@ module Bestiary
           HTML
           dom = parse_html(html)
           target_element = dom.at('p')
-
           parser = Parsers::Senses.new(dom)
 
-          expect(parser.parent_element).to eq(target_element)
+          result = parser.parent_element
+
+          expect(result).to eq(target_element)
         end
       end
 
@@ -24,11 +25,48 @@ module Bestiary
           HTML
           dom = parse_html(html)
           target_element = dom.at('p')
-
           parser = Parsers::Senses.new(dom)
 
-          expect(parser.parent_element).to eq(target_element)
+          result = parser.parent_element
+
+          expect(result).to eq(target_element)
         end
+      end
+    end
+
+    describe '#senses_text' do
+      it 'returns the text after "Senses"' do
+        dom = parse_html %(<p class = "stat-block-1"><b>Init</b> +3; <b>Senses</b> darkvision 120 ft.; <a href = "/pathfinderRPG/prd/coreRulebook/skills/perception.html#perception">Perception</a> +8</p>)
+        expected = ' darkvision 120 ft.; Perception +8'
+        parser = Parsers::Senses.new(nil)
+
+        result = parser.senses_text(dom)
+
+        expect(result).to eq(expected)
+      end
+    end
+
+    describe '#individual_senses' do
+      it 'returns senses split by separators' do
+        text = ' darkvision 120 ft.; Perception +8'
+        parser = Parsers::Senses.new(nil)
+
+        result = parser.individual_senses(text)
+
+        expect(result).to eq([' darkvision 120 ft.', ' Perception +8'])
+      end
+    end
+
+    describe '#sense_attributes' do
+      it 'returns a hash of attributes' do
+        sense_text = ' darkvision 120 ft.'
+        parser = Parsers::Senses.new(nil)
+
+        attributes = parser.sense_attributes(sense_text)
+
+        expect(attributes).to eq(title: 'Darkvision',
+                                 value: 120,
+                                 unit: 'ft.')
       end
     end
   end
