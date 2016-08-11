affb90b82fbfd5e72456f6eb4ee4c05f10b586a5
Edward Loveall <edward@edwardloveall.com>
2016-08-11 12:33:25 -0400

    Add armor parser
    This takes an element like this:
    <p class="stat-block-1"><b>AC</b> 21, touch 17, flat-footed 13 (+7 Dex,
    +1 dodge, +4 natural, –1 size)</p>
    
    And parses it into an array of Armor attributes like this:
    
    title: 'ac', bonus: 21
    title: 'touch', bonus: 17
    title: 'dex', bonus: 7
    title: 'size', bonus: -1
    etc.
    
    The way it works is by first finding the above element. It looks at all
    the stats and finds the first element starting with "AC ". Then it
    sanitizes the text to be lowercase, converts " (" and "; " into commas,
    removes
    ")", and converts en dashes "–" into hyphens "-".
    
    Then it has a string like: "ac 21, touch 17, flat-footed 13, +7 dex, +1
    dodge, +4 natural, –1 size". It splits the string by each comma so it
    has
    pairs like: "ac 21". Each pair then is scanned for digits "21", "+7",
    etc.,
    for the bonus and then the bonus is removed from the text, resulting in
    the title.
    
    Each value then is used to initialize a new Armor.

---
+++ lib/bestiary/parsers/armor.rb
@@ -0,0 +1,35 @@
+class Bestiary::Parsers::Armor
+  attr_reader :creature
+
+  def self.perform(creature)
+    new(creature).perform
+  end
+
+  def initialize(creature)
+    @creature = creature
+  end
+
+  def perform
+    text = parent_element.text
+    sanitized = text.downcase
+                    .sub(' (', ', ')
+                    .sub('; ', ', ')
+                    .sub(')', '')
+                    .sub('–', '-')
+    pairs = sanitized.split(', ')
+    pairs.map do |pair|
+      bonus = pair.scan(/[+\-]?\d+/).first
+      title = pair.sub(bonus, '').strip
+      Bestiary::Attributes::Armor.new(title: title, bonus: bonus.to_i)
+    end
+  end
+
+  def parent_element
+    @parent_element ||= begin
+      all_stats = creature.css('p.stat-block-1')
+      all_stats.detect do |stat|
+        stat.text.strip.match(/^AC /)
+      end
+    end
+  end
+end
---
+++ spec/parsers/armor_parser_spec.rb
@@ -0,0 +1,38 @@
+module Bestiary
+  RSpec.describe Parsers::Armor do
+    describe '.perform' do
+      it 'returns an array of Armor attributes' do
+        html = <<-HTML
+        <p class="stat-block-1"><b>AC</b> 19, touch 10, flat-footed 19 (+9 natural; +2 deflection vs. evil)</p>
+        HTML
+        dom = parse_html(html)
+        armors = [
+          Attributes::Armor.new(title: 'ac', bonus: 19),
+          Attributes::Armor.new(title: 'touch', bonus: 10),
+          Attributes::Armor.new(title: 'flat-footed', bonus: 19),
+          Attributes::Armor.new(title: 'natural', bonus: 9),
+          Attributes::Armor.new(title: 'deflection vs. evil', bonus: 2)
+        ]
+
+        result = Parsers::Armor.perform(dom)
+
+        expect(result).to match_array(armors)
+      end
+    end
+
+    describe '#parent_element' do
+      it 'returns the element containing all armor text' do
+        html = <<-HTML
+        <p class="stat-block-1"><b>AC</b> 21, touch 17, flat-footed 13 (+7 Dex, +1 dodge, +4 natural, –1 size)</p>
+        HTML
+        dom = parse_html(html)
+        stat = dom.at('p')
+        parser = Parsers::Armor.new(dom)
+
+        parent = parser.parent_element
+
+        expect(parent).to eq(stat)
+      end
+    end
+  end
+end
