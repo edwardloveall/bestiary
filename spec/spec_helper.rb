3ba6b50d8411a6a74a32656512abd6757285de94
Edward Loveall <edward@edwardloveall.com>
2016-08-15 14:26:34 -0400

    Add example persistence
    As of RSpec 3.3, the result of examples can be stored. This has the
    advantage of being able to know which examples failed last run so you
    can run just those example automatically with `rspec --only-failures`
    
    There's no reason to add this examples.txt file to version control
    however, so it is ignored
    
    More info:
    http://rspec.info/blog/2015/06/rspec-3-3-has-been-released/#core-new---o
    nly-failures-option

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,4 +10,5 @@ Dir['spec/support/**/*.rb'].each { |file| require File.expand_path(file) }
 RSpec.configure do |config|
   config.order = 'random'
   config.include Helpers
+  config.example_status_persistence_file_path = './spec/examples.txt'
 end
